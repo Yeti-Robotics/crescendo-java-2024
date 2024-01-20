68143d4c6f39075198ababb4f57bad2387ddac7f
vihaa <vihaansden@gmail.com>
2024-01-20 16:44:00 -0500

    PivotAimCommand initialize

--- src/main/java/frc/robot/commands/pivot/PivotAimCommand.java
+++ src/main/java/frc/robot/commands/pivot/PivotAimCommand.java
@@ -1,24 +1,33 @@
 package frc.robot.commands.pivot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-
+import frc.robot.subsystems.VisionSubsystem;
+import static frc.robot.constants.PivotConstants.*;
+import static frc.robot.constants.FieldConstants.*;
 
 public class PivotAimCommand extends Command {
-
-    public PivotAimCommand() {
-        // each subsystem used by the command must be passed into the
-        // addRequirements() method (which takes a vararg of Subsystem)
-        addRequirements();
+    private final VisionSubsystem visionSubsystem;
+    private double calcAngle;
+    private double relativePoseY;
+    private double relativePoseX;
+    private double robotPoseY;
+    private double robotPoseX;
+    public PivotAimCommand(VisionSubsystem visionSubsystem) {
+        this.visionSubsystem = visionSubsystem;
+        addRequirements(this.visionSubsystem);
     }
 
     @Override
     public void initialize() {
-
+        calcAngle = 0; //placeholder, set to bump value
+        relativePoseY = fieldLength - visionSubsystem.getPose2d().getY();
+        relativePoseX = speakerPose - visionSubsystem.getPose2d().getX();
+        robotPoseX = visionSubsystem.getPose2d().getX();
+        robotPoseY = visionSubsystem.getPose2d().getY();
     }
 
     @Override
     public void execute() {
-
     }
 
     @Override
