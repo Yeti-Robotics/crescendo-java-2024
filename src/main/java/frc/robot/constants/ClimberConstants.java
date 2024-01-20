cb9baf8fce188260fe656c36294fe838cc0a9995
adarsh-cms <adarshrtalusani@gmail.com>
2024-01-19 20:31:10 -0500

    ClimberSubsystem with a Servo braking mechanism. 2 motors that copy each other.

---
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public final class ClimberConstants {
+
+    public static final int CLIMBER_TALON_1 = 7;
+    public static final int CLIMBER_TALON_2 = 7;
+    public static final int SERVO_CHANNEL = 0;
+    public static final double CLIMB_SPEED = .4;
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,66 @@
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ClimberConstants;
+
+public class ClimberSubsystem extends SubsystemBase {
+    private final TalonFX climberFalcon1;
+    private final TalonFX climberFalcon2;
+    private final Servo climberBrake;
+
+    public ClimberSubsystem() {
+        climberFalcon1 = new TalonFX(ClimberConstants.CLIMBER_TALON_1);
+        climberFalcon2 = new TalonFX(ClimberConstants.CLIMBER_TALON_2);
+
+        climberBrake = new Servo(ClimberConstants.SERVO_CHANNEL);
+        climberFalcon1.setInverted(true);
+        climberFalcon2.setControl(new Follower(climberFalcon1.getDeviceID(), false));
+
+        var talon1Config = climberFalcon1.getConfigurator();
+        var talon2Config = climberFalcon2.getConfigurator();
+
+        var motorConfig = new TalonFXConfiguration();
+
+        motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        // motorConfig.SoftwareLimitSwitch = new SoftwareLimitSwitchConfigs()
+        motorConfig.FutureProofConfigs = true;
+    }
+
+    public void setClimberBrake(double servoPosition){
+        climberBrake.set(servoPosition);
+    }
+
+    @Override
+    public void periodic() {}
+
+    public void climbUp() {
+        climberFalcon1.set(ClimberConstants.CLIMB_SPEED);
+    }
+    public void climbDown() {
+        climberFalcon1.set(-ClimberConstants.CLIMB_SPEED);
+    }
+    public void stopClimb() {
+        climberFalcon1.set(0);
+    }
+    public double getLeftEncoder() {
+        return climberFalcon2.getRotorPosition().getValue();
+    }
+    public double getRightEncoder() {
+        return climberFalcon1.getRotorPosition().getValue();
+    }
+    public double getAverageEncoder() {
+        return (getLeftEncoder() + getRightEncoder()) / 2.0;
+    }
+    public boolean atEncoderLimit() {
+        return getAverageEncoder() <= ClimberConstants.CLIMBER_TALON_1;
+    }
+}
+
+
