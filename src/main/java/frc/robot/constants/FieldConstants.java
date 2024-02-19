037e2074d8db8504e32cbcbca8928153bfe5da8a
soccertanuj <101022128+soccertanuj@users.noreply.github.com>
2024-02-19 10:27:23 -0500

    updated field constants

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -26,15 +26,15 @@ import java.util.List;
  * Width refers to the <i>y</i> direction (as described by wpilib)
  */
 public class FieldConstants {
-    public static final double fieldLength = Units.inchesToMeters(651.223);
-    public static final double fieldWidth = Units.inchesToMeters(323.277);
-    public static final double wingX = Units.inchesToMeters(229.201);
-    public static final double podiumX = Units.inchesToMeters(126.75);
-    public static final double startingLineX = Units.inchesToMeters(74.111);
-    public static final double speakerPose = fieldWidth - Units.inchesToMeters(104.0);
+    public static double fieldLength = Units.inchesToMeters(651.223);
+    public static double fieldWidth = Units.inchesToMeters(323.277);
+    public static double wingX = Units.inchesToMeters(229.201);
+    public static double podiumX = Units.inchesToMeters(126.75);
+    public static double startingLineX = Units.inchesToMeters(74.111);
+
 
     public static final double robotHeight = Units.feetToMeters(2.333);
-    public static final double speakerHeightRelativeToBot = Units.inchesToMeters(77.9375) - robotHeight;
+    public static final double speakerHeightRelativeToBot = Speaker.centerSpeakerOpening.getY()- robotHeight;
 
     public static final Translation2d ampCenter =
             new Translation2d(Units.inchesToMeters(72.455), Units.inchesToMeters(322.996));
@@ -43,7 +43,7 @@ public class FieldConstants {
      * Staging locations for each note
      */
     public static final class StagingLocations {
-        public static final double centerlineX = Units.inchesToMeters(fieldLength / 2);
+        public static final double centerlineX = fieldLength / 2.0;
 
         // need to update
         public static final double centerlineFirstY = Units.inchesToMeters(29.638);
@@ -78,26 +78,24 @@ public class FieldConstants {
         /**
          * Center of the speaker opening (blue alliance)
          */
-        public static final Pose2d centerSpeakerOpening =
-                new Pose2d(fieldWidth - Units.inchesToMeters(104.0), Units.inchesToMeters(9), new Rotation2d());
-
-        // corners (blue alliance origin)
-        public static final Translation3d topRightSpeaker =
+        public static Translation3d topRightSpeaker =
                 new Translation3d(
                         Units.inchesToMeters(18.055),
                         Units.inchesToMeters(238.815),
-                        Units.inchesToMeters(13.091));
+                        Units.inchesToMeters(83.091));
 
-        public static final Translation3d topLeftSpeaker =
+        public static Translation3d topLeftSpeaker =
                 new Translation3d(
                         Units.inchesToMeters(18.055),
                         Units.inchesToMeters(197.765),
                         Units.inchesToMeters(83.091));
 
-        public static final Translation3d bottomRightSpeaker =
+        public static Translation3d bottomRightSpeaker =
                 new Translation3d(0.0, Units.inchesToMeters(238.815), Units.inchesToMeters(78.324));
-        public static final Translation3d bottomLeftSpeaker =
+        public static Translation3d bottomLeftSpeaker =
                 new Translation3d(0.0, Units.inchesToMeters(197.765), Units.inchesToMeters(78.324));
+        public static Translation3d centerSpeakerOpening =
+                bottomLeftSpeaker.interpolate(topRightSpeaker, 0.5);
 
         public static final double aprilTagWidth = Units.inchesToMeters(6.50);
         public static final AprilTagFieldLayout aprilTags;
