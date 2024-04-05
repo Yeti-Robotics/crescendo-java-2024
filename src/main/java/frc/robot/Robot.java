8966e28de0fa2b959b9c253fa8c0a924386bb40f
yetiDS <yeti.driverstation@gmail.com>
2024-04-04 21:09:06 -0400

    Incomplete source side auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -72,7 +72,8 @@ public class Robot extends TimedRobot {
         autoChooser.addOption(AutoConstants.AutoModes.SOURCE_SIDE_3_PIECE.name, AutoConstants.AutoModes.SOURCE_SIDE_3_PIECE);
 //        autoChooser.addOption(AutoConstants.AutoModes.AMP_4_TWO_PIECE.name, AutoConstants.AutoModes.AMP_4_TWO_PIECE);
         autoChooser.addOption(AutoConstants.AutoModes.SOURCE_SIDE_SHUTTLE_AMP.name, AutoConstants.AutoModes.SOURCE_SIDE_SHUTTLE_AMP);
-        autoChooser.addOption(AutoConstants.AutoModes.SHUTTLE_3_SOURCE.name, AutoConstants.AutoModes.SHUTTLE_3_SOURCE);
+        autoChooser.addOption(AutoConstants.AutoModes.SOURCE_SIDE_SHUTTLE_AMP.name, AutoConstants.AutoModes.SOURCE_SIDE_SHUTTLE_AMP);
+        autoChooser.addOption(AutoConstants.AutoModes.SOURCE_SIDE_BYPASS.name, AutoConstants.AutoModes.SOURCE_SIDE_BYPASS);
 
         SmartDashboard.putData("Auto Chooser", autoChooser);
         previousSelectedAuto = autoChooser.getSelected();
