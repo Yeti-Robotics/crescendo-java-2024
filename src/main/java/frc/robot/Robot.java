018116fcd0f662c33f51a97730b2da0b204582aa
ka930 <77208047+ka930@users.noreply.github.com>
2024-07-13 10:18:15 -0400

    robot.java: slightly more aggressively clean up the unused commented code, along with formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,26 +15,21 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.auto.AutoNamedCommands;
+import frc.robot.constants.AutoConstants;
 import frc.robot.constants.VisionConstants;
 import frc.robot.util.LimelightHelpers;
-import frc.robot.constants.AutoConstants;
 
 public class Robot extends TimedRobot {
-    private Command autonomousCommand;
     private static RobotContainer robotContainer;
-
+    private static SendableChooser<AutoConstants.AutoMode> autoChooser;
+    LimelightHelpers.Results lastResult;
+    private Command autonomousCommand;
     private AutoNamedCommands namedCommands;
     private AutoConstants.AutoMode previousSelectedAuto;
-
-    LimelightHelpers.Results lastResult;
-    private static SendableChooser<AutoConstants.AutoMode> autoChooser;
-
 
     @Override
     public void robotInit() {
         robotContainer = new RobotContainer();
-//        SignalLogger.enableAutoLogging(true);
-        System.out.println("ROBOT INIT START");
 
         namedCommands = new AutoNamedCommands(robotContainer.intakeSubsystem, robotContainer.shooterSubsystem, robotContainer.pivotSubsystem, robotContainer.armSubsystem);
         namedCommands.registerCommands();
@@ -44,7 +39,7 @@ public class Robot extends TimedRobot {
         for (var autoMode : AutoConstants.AutoMode.values()) {
             autoChooser.addOption(autoMode.name, autoMode);
         }
-
+        // note: setDefaultOption overwrites the name in the map, so we won't have duplicate options
         autoChooser.setDefaultOption(AutoConstants.AutoMode.BUMP_ONLY.name, AutoConstants.AutoMode.BUMP_ONLY);
 
         SmartDashboard.putData("Auto Chooser", autoChooser);
@@ -55,25 +50,18 @@ public class Robot extends TimedRobot {
 
         DataLogManager.start();
         DriverStation.startDataLog(DataLogManager.getLog());
-        System.out.println("ROBOT INIT END");
         SmartDashboard.putNumber("shooterstate-position", 0.5);
     }
 
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
-       lastResult = LimelightHelpers.getLatestResults("limelight").targetingResults;
-//       DriverStation.refreshData();
-
+        lastResult = LimelightHelpers.getLatestResults("limelight").targetingResults;
     }
 
     @Override
     public void disabledInit() {
         LimelightHelpers.setLEDMode_ForceOff(VisionConstants.LIMELIGHT_NAME);
-//        resetCommandsAndButons();
-
-//        Command disabledInitAuto = new PathPlannerAuto("bumpOnly").ignoringDisable(true);
-//        disabledInitAuto.schedule();
     }
 
     @Override
@@ -83,36 +71,16 @@ public class Robot extends TimedRobot {
             autonomousCommand = AutoBuilder.buildAuto(previousSelectedAuto.name);
         }
 
-//        System.out.println(robotContainer.shooterSubsystem.getBeamBreak());
-//        System.out.println(previousSelectedAuto.name);
-//        System.out.println(robotContainer.intakeSubsystem.getBeamBreak());
-
-
-//        var lastResult = LimelightHelpers.getLatestResults("limelight").targetingResults;
-//        if (DriverStation.getAlliance().isPresent()) {
-//            if (DriverStation.getAlliance().get() == (DriverStation.Alliance.Red)) {
-//                Pose2d llPose = lastResult.getBotPose2d_wpiRed();
-//                if (lastResult.valid) {
-//                    robotContainer.drivetrain.addVisionMeasurement(llPose, Timer.getFPGATimestamp());
-//                }
-//            } else {
-//                Pose2d llPose = lastResult.getBotPose2d_wpiBlue();
-//                if (lastResult.valid) {
-//                    robotContainer.drivetrain.addVisionMeasurement(llPose, Timer.getFPGATimestamp());
-//                }
-//            }
-//        }
-
         System.out.println(previousSelectedAuto.name);
     }
 
     @Override
     public void disabledExit() {
+
     }
 
     @Override
     public void autonomousInit() {
-//        resetCommandsAndButons();
         autonomousCommand.schedule();
     }
 
@@ -122,35 +90,26 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousExit() {
-//        resetCommandsAndButons();
         autonomousCommand.cancel();
     }
 
     @Override
     public void teleopInit() {
         robotContainer.eventLoop.poll();
-//        resetCommandsAndButons();
         if (autonomousCommand != null) {
             autonomousCommand.cancel();
         }
-
-//        LimelightHelpers.setLEDMode_ForceOff(VisionConstants.LIMELIGHT_NAME);
-
-//        SignalLogger.start();
     }
 
     @Override
     public void teleopPeriodic() {
-
         if (DriverStation.getAlliance().isPresent()) {
             if (lastResult != null && lastResult.valid && Math.abs(robotContainer.drivetrain.getPigeon2().getRate()) < 230) {
                 Pose2d llPose = lastResult.getBotPose2d_wpiBlue();
                 robotContainer.drivetrain.addVisionMeasurement(llPose, Timer.getFPGATimestamp());
             }
         }
-
     }
-
 
 
     @Override
