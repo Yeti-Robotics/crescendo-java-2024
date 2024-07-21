bd260f7d63bb73654b878f74bc736bd6899adab6
boomermath <boomermath13@gmail.com>
2024-07-20 21:55:53 -0400

    +1

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -29,7 +29,7 @@ public class VisionSubsystem extends SubsystemBase {
         SequentialCommandGroup blinkCommands = new SequentialCommandGroup();
         blinkCommands.addRequirements(this);
 
-        for (int i = 0; i < VisionConstants.NUM_BLINKS - 1; i++) {
+        for (int i = 0; i < VisionConstants.NUM_BLINKS; i++) {
             blinkCommands.addCommands(Commands.sequence(
                     ledOn(),
                     new WaitCommand(VisionConstants.BLINK_DELAY),
